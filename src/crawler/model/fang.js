13419ee8df2e075b136db950a4c63a25455af41c
june <june@mazing.com>
2017-05-18 16:51:54 +0800

    转utf-8

--- src/crawler/model/fang.js
+++ src/crawler/model/fang.js
@@ -4,19 +4,4 @@
  */
 const Base = requireBaseModel();
 module.exports = class extends Base {
-  // 构造函数
-  init(...args) {
-    super.init(...args);
-  }
-
-  /**
-   * 保存数据
-   * @param data 一个json格式的对象
-   */
-  async save(data) {
-    const rs = await this._model.create(data);
-    this.LOG.warn(`rs: ${rs}`);
-    const row = await this._model.findOne({ name: 'ruanzhijun' });
-    this.LOG.warn(`row: ${row}`);
-  }
 };
--- src/crawler/service/fang.js
+++ src/crawler/service/fang.js
@@ -1,6 +1,7 @@
 const Base = think.service('base', 'common');
-// const axios = require('axios');
-// const cheerio = require('cheerio');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
 
 module.exports = class extends Base {
   // 最先执行
@@ -11,12 +12,15 @@ module.exports = class extends Base {
   /**
    * 爬虫入口
    */
-  crawler() {
-    this.LOG.warn('房天下。。。房天下。。。房天下。。。房天下。。。');
+  async crawler() {
+    await this.city();
   }
 
-  show() {
-    const fang1 = this.model('fang');
-    fang1.save({ xxx: 1, tttt: 43 });
+  async city() {
+    const url = `http://fang.com/SoufunFamily.htm?t=${moment.now()}`;
+    const data = await axios.get(url);
+    // const $ = cheerio.load(data.data);
+    // console.log($('.letterSelt').html());
+    console.log(data.data);
   }
 };
