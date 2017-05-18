7f698567da9cd5b034c485003859779a73640098
june <june@mazing.com>
2017-05-18 10:20:55 +0800

    微信小程序userinfo结构

--- src/api/controller/index.js
+++ src/api/controller/index.js
@@ -2,5 +2,19 @@ const Base = requireBaseController();
 module.exports = class extends Base {
   init(...args) {
     super.init(...args);
+
+    /*
+
+    // 小程序解到的userinfo
+    { nickName: '小丸子',
+      gender: 1,
+      language: 'zh_CN',
+      city: 'Guangzhou',
+      province: 'Guangdong',
+      country: 'CN',
+      avatarUrl: 'http://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqSf82W4I6bFLIr40ko7VzNrPwicicVKpO0Vq9vscgfzuzeSxZ4Uic8Nu2Zzib1hnRJWLVfP6Y4GbdD4g/0'
+    }
+
+    */
   }
 };
